efbd944969f19dccf60ceb226d936bb557a6bbd8
Tony <tony.inuma@gmail.com>
2022-12-10 09:42:39 -0500

    Feat: Middleware Morgan to watch requests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express, { Application } from "express";
 import authRoutes from "./routes/authRoutes";
+import morgan from "morgan";
 
 const app: Application = express();
 
+/**
+ * Middlewares
+ */
+app.use(morgan('dev'));
+
+/**
+ * Routes
+ */
 app.use(authRoutes);
 
 export default app;
