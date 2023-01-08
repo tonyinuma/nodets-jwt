54d603837b5d3a4444d4fae2085fd079bc8f11ae
Tony <tony.inuma@gmail.com>
2023-01-08 14:09:01 -0500

    Fix: user model Interface

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
-export interface IUser {
+export interface IUser extends Document {
     username: string,
     email: string,
     password: string
